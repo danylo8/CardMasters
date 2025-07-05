f1e13cba2da4bc6974234ba0252e64434c03d2c1
danylo8 <ozai.avatar@gmail.com>
2025-07-04 22:33:45 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 const totalImages = 44;
-const cols = 18;
-const rows = 10;
+const cols = 25;
+const rows = 16;
 const container = document.getElementById('bg-grid');
 
 for (let i = 0; i < cols * rows; i++) {
